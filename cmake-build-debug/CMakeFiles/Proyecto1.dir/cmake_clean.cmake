6e7aa1d60e433eb7334606aad00f565b281a91ca
Andres Arias <andres.arias12@gmail.com>
2017-03-09 21:18:01 -0600

    Agregado codigo del profe

---
+++ cmake-build-debug/CMakeFiles/Proyecto1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1.dir/main.cpp.o"
+  "Proyecto1.pdb"
+  "Proyecto1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/functor.h
+++ src/functor.h
@@ -5,4 +5,56 @@
 #ifndef META_FUNCTOR_H
 #define META_FUNCTOR_H
 
+namespace anpi {
+
+    template<typename T>
+    class ln_a {
+        private:
+            // Coeficientes de la serie de Taylor.
+            T* _coef;
+
+            // Alineacion de la memoria en bytes.
+            static const unsigned int _alignment = 32u;
+            
+                // Centro de la serie
+            T _center;
+
+            /**
+             * Inicializa los coeficientes de la serie de Taylor centrada en
+             * _center para el numero dado de coeficientes.
+             */
+            void init(const T center, const unsigned int terms) {
+                _center = center;
+                // Redondear hacia arriba.
+                unsigned int blocks = ((terms * sizeof(T) + (_alignment - 1)) / _alignment);
+                _coef = static_cast<T*>(aligned_alloc(_alignment, blocks * _alignment)); 
+
+                ln<T> logn;k
+                for (unsigned int i = 0 ; i < terms ; ++i) {
+                    _coef[i] = logn.diff(center, i) / factorial<T>(i);
+                }
+            }
+        
+        public:
+            ln_a(const T center, const unsigned int terms) {
+                init(center, terms);
+            }
+            
+            // Destructor libera memoria reservada.
+            ~ln_a() { free(_coef); }
+            
+            // Evaluacion de la funcion logaritmica.
+            inline T operador()(const T val) const {
+                T h = val - _center;
+                T result; //evaluar de alguna forma
+                return result;
+            }
+
+            // Evaluacion de la n-esima derivada.
+            inline T diff(const T x, const unsigned int n) {
+                T result; //evaluar de alguna forma.
+                return result;
+            }
+    };
+}
 #endif //META_FUNCTOR_H
