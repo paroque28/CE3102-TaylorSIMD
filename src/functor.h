b5f21bcea0a416e417149681b7c5201807ff311e
Pablo Rodriguez <paroque28@gmail.com>
2017-03-09 22:02:52 -0600

    "Agregado el functor WIP"

--- src/functor.h
+++ src/functor.h
@@ -5,6 +5,10 @@
 #ifndef META_FUNCTOR_H
 #define META_FUNCTOR_H
 
+#include <malloc.h>
+#include "alloc.h"
+#include "meta.h"
+#include "algo.h"
 namespace anpi {
 
     template<typename T>
@@ -12,26 +16,23 @@ namespace anpi {
         private:
             // Coeficientes de la serie de Taylor.
             T* _coef;
-
-            // Alineacion de la memoria en bytes.
-            static const unsigned int _alignment = 32u;
-            
                 // Centro de la serie
             T _center;
 
+            int _terms;
             /**
              * Inicializa los coeficientes de la serie de Taylor centrada en
              * _center para el numero dado de coeficientes.
              */
             void init(const T center, const unsigned int terms) {
-                _center = center;
-                // Redondear hacia arriba.
-                unsigned int blocks = ((terms * sizeof(T) + (_alignment - 1)) / _alignment);
-                _coef = static_cast<T*>(aligned_alloc(_alignment, blocks * _alignment)); 
+                a_log<center,terms> log;
 
-                ln<T> logn;k
-                for (unsigned int i = 0 ; i < terms ; ++i) {
-                    _coef[i] = logn.diff(center, i) / factorial<T>(i);
+                T * b = reinterpret_cast<double*>(&log);
+                _center = center;
+               _coef = (T*) malloc_simd(sizeof(T)*terms);
+                _terms  = terms;
+                for (int i = 0; i < terms; ++i) {
+                    *(_coef+i) = *(b+1);
                 }
             }
         
@@ -46,7 +47,7 @@ namespace anpi {
             // Evaluacion de la funcion logaritmica.
             inline T operador()(const T val) const {
                 T h = val - _center;
-                T result; //evaluar de alguna forma
+                T result = anpi::ref::estrins(h,_coef,_terms); //evaluar de alguna forma
                 return result;
             }
 
