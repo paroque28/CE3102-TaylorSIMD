393549bd26491609a92da136d91f2d4e9250fe22
Pablo Rodriguez <paroque28@gmail.com>
2017-03-10 21:00:41 -0600

    listo con pruebas de error

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@
 
 //#define HORNER
 #define __linux__
+
+
 using namespace anpi;
+
+
 template <typename T>
 int initVector(T* a, unsigned int l)
 {
@@ -17,23 +21,22 @@ int initVector(T* a, unsigned int l)
 }
 int main() {
     std::cout.precision(100);
-    int l =10;
-    float x = 2.0f;
-    float * a =(float*)(malloc_simd(l*sizeof(float)));
-    initVector(a,l);
 
 
-    anpi::ref::ln_a<double> ref;
+    ref::ln_a<double> ref;
     ref.init<10,5>();//centro 10, terms 5
-    anpi::opt::ln_a<double> opt;
+    opt::ln_a<double> opt;
     opt.init<10,5>();//centro 10, terms 5
 
     std::cout << ref(10) << std::endl;
 
     std::cout << opt(10) << std::endl;
-
-    tests::time(ref,25000,10);
+    //medir tiempos
+    tests::time(ref,25000,10); //25000 pruebas empezando en 10
     tests::time(opt,25000,10);
+
+    //medir error
+    tests::error(ref,5,10,&std::log);
 
     return 0;
 }
